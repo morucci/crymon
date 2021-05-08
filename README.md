24e315ce829bd9fd1a91d1d519a9b08b67fdacd4
Fabien Boucher <fboucher@redhat.com>
2021-05-08 16:01:36 +0000

    Initial summon

---
+++ README.md
@@ -0,0 +1,6 @@
+# crymon
+
+[![Hackage](https://img.shields.io/hackage/v/crymon.svg?logo=haskell)](https://hackage.haskell.org/package/crymon)
+[![MIT license](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+A tool to compute asset value from exchanges
---
+++ src/Crymon.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2021 Fabien Boucher
+SPDX-License-Identifier: MIT
+Maintainer: Fabien Boucher <fabien.dot.boucher@gmail.com>
+
+A tool to compute asset value from exchanges
+-}
+
+module Crymon
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
